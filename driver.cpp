88d8aa515c56a2ed666448af8e2480cd81c79d88
georgeo <georgeosabu@gmail.com>
2020-04-20 13:20:31 +0200

    handles the inputs commands correctly

--- driver.cpp
+++ driver.cpp
@@ -8,23 +8,86 @@ int main(int argc, char *argv[])
     string outputFile;
     int noOfClusters;
     int histogramWidth;
-    if (argc>2)
+    if (argc > 2)
     {
         string s(argv[2]);
-        if (s=="-o")
+        if (s == "-o")
         {
-            outputFile=argv[3];
-            
+            outputFile = argv[3];
+            if (argc > 4)
+            {
+                string ss(argv[4]);
+                if (ss == "-k")
+                {
+                    noOfClusters = stoi(argv[5]);
+                    if (argc > 6)
+                    {
+                        string sss = argv[6];
+                        if (sss == "-bin")
+                        {
+                            histogramWidth = stoi(argv[7]);
+                        }
+                        else
+                        {
+                            cout << "invalid input" << endl;
+                        }
+                    }
+                    else
+                    {
+                        histogramWidth = 1;
+                    }
+                }
+                else if (ss == "-bin")
+                {
+                    noOfClusters = 10;
+                    histogramWidth = stoi(argv[5]);
+                }
+                else
+                {
+                    cout << "invalid input" << endl;
+                }
+            }
+            else
+            {
+                noOfClusters = 10;
+                histogramWidth = 1;
+            }
         }
-        else if(s=="-k"){
-            noOfClusters=stoi(argv[3]);
+        else if (s == "-k")
+        {
+            outputFile = "toConsoleDefault";
+            noOfClusters = stoi(argv[3]);
+            if (argc > 4)
+            {
+                string ks(argv[4]);
+                if (ks == "-bin")
+                {
+                    histogramWidth = stoi(argv[5]);
+                }
+                else
+                {
+                    cout << "invalid input" << endl;
+                }
+            }
+            else
+            {
+                histogramWidth = 1;
+            }
         }
-        else if( s=="-bin"){
-            histogramWidth=stoi(argv[3]);
+        else if (s == "-bin")
+        {
+            outputFile = "toConsoleDefault";
+            noOfClusters = 10;
+            histogramWidth = stoi(argv[3]);
         }
-        
     }
-    cout<<outputFile;
-    cout<<noOfClusters;
-    
+    else
+    {
+        outputFile = "toConsoleDefault";
+        noOfClusters = 10;
+        histogramWidth = 1;
+    }
+    cout << outputFile << endl;
+    cout << noOfClusters << endl;
+    cout << histogramWidth << endl;
 }
