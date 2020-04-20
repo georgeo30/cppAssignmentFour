829f042ac958ed5393646c674831bed5bec417a0
georgeo <georgeosabu@gmail.com>
2020-04-20 12:08:13 +0200

    initial commit

---
+++ Cluster.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include "cluster.h"
+#include <sstream>
+
+using namespace std;
+namespace THNGEO002
+{
+
+Cluster::Cluster()
+{
+   
+} // default constructor
+Cluster::~Cluster()
+{
+  
+
+} // destructor
+}
---
+++ cluster.h
@@ -0,0 +1,24 @@
+#ifndef _CLUSTER_H
+#define _CLUSTER_H
+#include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+namespace THNGEO002
+{
+    using namespace std;
+
+class Cluster
+{
+private:                                  // private members
+    
+public:                                   // public members
+    Cluster();                           // default constructor - define in .cpp
+    ~Cluster();                          // destructor - define in .cpp file
+   
+};
+
+} // namespace THNGEO002
+#endif
